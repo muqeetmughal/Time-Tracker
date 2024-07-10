512f560b8d3f123c590284803ff46b3ffb40c195
muqeetmughal <muqeetmughal786@gmail.com>
2024-07-11 00:44:41 +0500

    vercel config updated

--- vercel_build.sh
+++ vercel_build.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 # Exit on error
 set -o errexit
+# create a virtual environment named 'venv' if it doesn't already exist
+python3.11 -m venv venv
 
+# activate the virtual environment
+source venv/bin/activate
 # Modify this line as needed for your package manager (pip, poetry, etc.)
 pip install -r requirements.txt
 
